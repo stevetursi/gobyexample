2eae6ec31bfc280999cdacda9be7e3b6e164ef89
Steven Tursi <stevetursi@gmail.com>
2016-03-18 20:04:35 -0400

    pointers

---
+++ 017-Pointers/pointers.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	i:= 1
+	fmt.Println("Initial:",i)
+	fmt.Println("Zeroval:",i)
+	fmt.Println("zeroptr:",i)
+	fmt.Println("pointer:",&i)
+
+	zeroval(i)
+	fmt.Println("zeroval:",i)
+	fmt.Println("zeroptr:",i)
+	fmt.Println("pointer:",&i)
+
+	zeroptr(&i)
+	fmt.Println("zeroval:",i)
+	fmt.Println("zeroptr:",i)
+	fmt.Println("pointer:",&i)
+
+}
