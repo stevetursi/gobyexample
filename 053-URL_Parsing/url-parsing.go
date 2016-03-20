26f50e1f3c335ddeaebbf7deca0b79e7115da4e3
Steven Tursi <stevetursi@gmail.com>
2016-03-20 09:39:49 -0400

    URL Parsing

---
+++ 053-URL_Parsing/url-parsing.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+)
+
+func main() {
+	p := fmt.Println
+
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+
+	p(u.Scheme)
+
+	p(u.User)
+	p(u.User.Username())
+	pw, _ := u.User.Password()
+	p(pw)
+
+	p(u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	p(host)
+	p(port)
+
+	p(u.Path)
+	p(u.Fragment)
+
+	p(u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	p(m)
+	p(m["k"][0])
+
+}
